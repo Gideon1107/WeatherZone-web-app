c1b20e5aed9f59baa556be7b696dfa80701d70c4
Gideon1107 <ayenigideon1107@gmail.com>
2024-05-06 18:50:19 +0100

    front-end and first api call done

--- index.js
+++ index.js
@@ -4,13 +4,37 @@ import bodyParser from "body-parser"
 
 const app = express();
 const port = 3000;
-const API_URL = "";
+const  GEO_API_URL  = "http://api.openweathermap.org/geo/1.0/direct";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 
 app.get("/", (req, res) => {
+    res.render("index.ejs")
+});
+
+app.post("/search", async (req, res) => {
+    const params = {
+        q: req.body.query,
+        appid: "287c09b3cea989a71939eb3692e22768"
+    }
+    try {
+        const response = await axios.get(GEO_API_URL, {params: params});
+
+        const result = response.data;
+        console.log(result[0]["lat"]);
+
+    //     res.render("index.ejs", {
+    //         secret: result.secret,
+    //         user: result.username
+    // });
+    } catch(error) {
+        console.log(error.response.data)
+        res.status(500)
+    }
+
+    console.log(req.body.query);
     res.render("index.ejs")
 });
 
